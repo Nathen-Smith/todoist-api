64ba421f1d87da6d7c37c24f222cace06132dfdd
Jinda Han <hanjinda@gmail.com>
2020-03-07 20:47:27 +0000

    Update secrets.js

--- config/secrets.js
+++ config/secrets.js
@@ -4,5 +4,5 @@
 module.exports = {
     token : "secret-starter-mern",
     mongo_connection : "YOUR URL TO MONGODB"
-    //example: mongo_connection : "mongodb+srv://[type-yours]:[type-yours]@[type-yours].mongodb.net/test?retryWrites=true"
+    //example: mongo_connection : "mongodb+srv://[type-yours]:[type-yours]@[type-yours-web-provided].mongodb.net/test?retryWrites=true"
 };
