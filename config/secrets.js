ec364686c4061fbf9075d575ef1eeea2cbaba478
Jinda Han <hanjinda@gmail.com>
2020-03-07 14:16:49 -0600

    initial mp3

---
+++ config/secrets.js
@@ -0,0 +1,8 @@
+/*
+ * You generally want to .gitignore this file to prevent important credentials from being stored on your public repo.
+ * example: mongo_connection : "mongodb+srv://[type-yours]:[type-yours]@[type-yours].mongodb.net/test?retryWrites=true"
+ */
+module.exports = {
+    token : "secret-starter-mern",
+    mongo_connection : "YOUR URL TO MONGODB"
+};
