d8365fd7dff3d956d71c7535ad6ffd1f928a3d87
Ali Zaidi <azaidi3@wisc.edu>
2021-10-13 16:14:31 -0500

    commented out line for Mongoose connection string

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var app = express();
 // Use environment defined port or 4000
 var port = process.env.PORT || 4000;
 
-// Connect to a MongoDB
-mongoose.connect(secrets.mongo_connection,  { useNewUrlParser: true });
+// Connect to a MongoDB --> Uncomment this once you have a connection string!!
+//mongoose.connect(secrets.mongo_connection,  { useNewUrlParser: true });
 
 // Allow CORS so that backend and frontend could be put on different servers
 var allowCrossDomain = function (req, res, next) {
